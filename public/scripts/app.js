e6589df5a9426b78078b7a551d2a06029a0a541d
Thomas <thomwe8@hotmail.com>
2018-04-10 19:05:09 -0300

    exploring jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,61 @@ console.log('App.js is running');
 
 // JSX - Javascript XML
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+
+// create a templateTwo var JSX expression
+// div
+//  h1 -> Thomas
+//  p  -> Age: 25
+//  p  -> Location: Florianópolis
+//  Render templateTwo instead of template
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Thomas'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 25'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Florian\xF3polis'
+    )
 );
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,32 @@
 console.log('App.js is running');
 
 // JSX - Javascript XML
-let template = <h1>Indecision App</h1>;
+let template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+// create a templateTwo var JSX expression
+// div
+//  h1 -> Thomas
+//  p  -> Age: 25
+//  p  -> Location: Florianópolis
+//  Render templateTwo instead of template
+
+let templateTwo = (
+    <div>
+        <h1>Thomas</h1>
+        <p>Age: 25</p>
+        <p>Location: Florianópolis</p>
+    </div>
+);
 
 let appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+ReactDOM.render(templateTwo, appRoot);
