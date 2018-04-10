f1f148ae1ab11f16ef4f028ba8dce09bdf3ce260
Thomas <thomwe8@hotmail.com>
2018-04-10 18:43:56 -0300

    babel working and running

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # IndecisionApp
 App created through the Udemy Course: The Complete React Web Developer Course (with Redux) by Andrew Mead
 https://www.udemy.com/react-2nd-edition
+
+Use yarn install to install dependencies
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 console.log('App.js is running');
 
 // JSX - Javascript XML
-let template = <p>This is JSX from app.js</p>;
+let template = <h1>Indecision App</h1>;
 
 let appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
