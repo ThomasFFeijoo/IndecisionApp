b9d76e09a29d5ee4dda2a0b73750a9e65936f6b8
Thomas <thomwe8@hotmail.com>
2018-04-11 17:27:54 -0300

    es6 classes: part 2 subclasses

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -19,9 +19,63 @@ class Person {
     }
 }
 
-const me = new Person('Thomas Feijoo', 25);
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor() {
+        // !! converts to boolean
+        return !!this.major;
+    }
+
+    getDescription() {
+        let description = super.getDescription();
+        if(this.hasMajor()) {
+            description += ` Their major is ${this.major}`;
+        }
+
+        return description;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    hasHomeLocation() {
+        return !!this.homeLocation;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+        if(this.hasHomeLocation()) {
+            greeting += ` I'm visiting from ${this.homeLocation}`;
+        }
+
+        return greeting;
+    }
+}
+
+
+
+// Traveler -> Person
+// Add support for homeLocation
+// Override getGreeting
+// 1. Hi. I am 'Name'. I'm visiting from 'homeLocation'
+// 2. Hi. I am 'Name'
+
+
+const me = new Student('Thomas Feijoo', 25, 'Computer Science');
+const other = new Student();
+const traveler = new Traveler('Mochileiro', 30, 'Santos');
+const anontraveler = new Traveler();
 
 console.log(me.getDescription());
-
-const other = new Person();
 console.log(other.getDescription());
+
+console.log(traveler.getGreeting());
+console.log(anontraveler.getGreeting());
