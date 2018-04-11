b0d25a894e4550ffd47f63a22cf6fec54282c059
Thomas <thomwe8@hotmail.com>
2018-04-11 18:16:49 -0300

    creating a react component

---
+++ src/app.js
@@ -0,0 +1,57 @@
+class Header extends React.Component {
+    render() {
+        return (
+          <div>
+              <h1>Indecision</h1>
+              <h2>Put your life in the hands of a computer</h2>
+          </div>
+        );
+    }
+}
+
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        );
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <ol>
+                    <li>Option 1</li>
+                    <li>Option 2</li>
+                </ol>
+            </div>
+        );
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" name="option"/>
+                    <button>Add Option</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+const jsx = (
+    <div>
+        <Header/>
+        <Action/>
+        <Options/>
+        <AddOption/>
+    </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
