618c720c4d17f3792c0762f7b59f41bd9f2b19ff
Thomas <thomwe8@hotmail.com>
2018-04-14 22:27:52 -0300

    adding state to counter app, part 1

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,3 +1,38 @@
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+    }
+    handleAddOne() {
+        console.log('handleAddOne');
+    }
+
+    handleMinusOne() {
+        console.log('handleMinusOne');
+    }
+
+    handleReset() {
+        console.log('handleReset');
+    }
+
+    render() {
+        return (
+          <div>
+              <h1>Count: </h1>
+              <button onClick={this.handleAddOne}>+1</button>
+              <button onClick={this.handleMinusOne}>-1</button>
+              <button onClick={this.handleReset}>reset</button>
+          </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<Counter/>, document.getElementById('app'));
+
+/*
 let count = 0;
 // arrow function
 const addOne = () => {
@@ -30,4 +65,4 @@ const renderCounterApp = () => {
     ReactDOM.render(templateTwo, appRoot);
 };
 
-renderCounterApp();
+renderCounterApp();*/
