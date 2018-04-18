900f8ae1a3059a27e051a906cebe2e0b6442ad7d
Thomas <thomwe8@hotmail.com>
2018-04-18 10:29:56 -0300

    setting up babel with webpack

--- src/app.js
+++ src/app.js
@@ -8,6 +8,6 @@ console.log(validator.isEmail('test@hehe.com'));*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const template = React.createElement('p', {}, 'testing');
+const template = <p>testing jsx</p>;
 
 ReactDOM.render(template, document.getElementById('app'));
