d958d66f476793fa097acc4397eaa5338e9bf060
Thomas <thomwe8@hotmail.com>
2018-04-18 10:51:15 -0300

    one component per file

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
-// install -> import -> use
-
-/*
-import validator from 'validator';
-
-console.log(validator.isEmail('test@hehe.com'));*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const template = <p>testing jsx</p>;
+import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(template, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
