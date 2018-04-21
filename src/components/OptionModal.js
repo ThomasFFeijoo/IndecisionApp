ec3566ac13a8414dc56c7d8b32e68981a936b16f
Thomas <thomwe8@hotmail.com>
2018-04-21 16:06:56 -0300

    styling react modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,12 @@ const Optionmodal = (props) => (
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleClearSelectedOption} // function executed when click outside of the box, or esc
         contentLabel="Selected Option"
+        closeTimeoutMS={200}
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClearSelectedOption}>Okay</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleClearSelectedOption}>Okay</button>
     </Modal>
 );
 
